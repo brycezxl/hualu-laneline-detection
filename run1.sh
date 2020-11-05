1470a03f9a4517221fe326a71e7ac48c9a9d0be3
brycezxl <43881818+brycezxl@users.noreply.github.com>
2020-11-05 10:40:03 +0800

    1

--- run1.sh
+++ run1.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-city-1.yaml --do_eval | tee -a ./train_log/city1
-CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-city-2.yaml --do_eval | tee -a ./train_log/city2
-CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-city-3.yaml --do_eval | tee -a ./train_log/city3
+CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-city-1.yaml --do_eval | tee -a ./runs/city1
+CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-city-2.yaml --do_eval | tee -a ./runs/city2
+CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-city-3.yaml --do_eval | tee -a ./runs/city3
--- run2.sh
+++ run2.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=1 python pdseg/train.py --cfg ./configs/sf-hr18-image-1.yaml --do_eval | tee -a ./train_log/img1
-CUDA_VISIBLE_DEVICES=1 python pdseg/train.py --cfg ./configs/sf-hr18-image-2.yaml --do_eval | tee -a ./train_log/img2
-CUDA_VISIBLE_DEVICES=1 python pdseg/train.py --cfg ./configs/sf-hr18-image-3.yaml --do_eval | tee -a ./train_log/img3
+CUDA_VISIBLE_DEVICES=1 python pdseg/train.py --cfg ./configs/sf-hr18-image-1.yaml --do_eval | tee -a ./runs/img1
+CUDA_VISIBLE_DEVICES=1 python pdseg/train.py --cfg ./configs/sf-hr18-image-2.yaml --do_eval | tee -a ./runs/img2
+CUDA_VISIBLE_DEVICES=1 python pdseg/train.py --cfg ./configs/sf-hr18-image-3.yaml --do_eval | tee -a ./runs/img3
