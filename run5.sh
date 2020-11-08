caeb8567234e2bee0e41c72dc86179d941c5e99d
brycezxl <43881818+brycezxl@users.noreply.github.com>
2020-11-08 10:29:14 +0800

    1

--- run5.sh
+++ run5.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-norm-4.yaml --do_eval | tee -a ./runs/norm4
-CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-norm-3.yaml --do_eval | tee -a ./runs/norm3
+CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-size-11.yaml --do_eval | tee -a ./runs/size10
+CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-size-10.yaml --do_eval | tee -a ./runs/size11
 
 # CUDA_VISIBLE_DEVICES=0 python pdseg/vis.py --cfg ./configs/sf-hr18-city-1.yaml --use_gpu
--- run6.sh
+++ run6.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1 python pdseg/train.py --cfg ./configs/sf-hr18-norm-1.yaml --do_eval | tee -a ./runs/norm1
-CUDA_VISIBLE_DEVICES=1 python pdseg/train.py --cfg ./configs/sf-hr18-norm-2.yaml --do_eval | tee -a ./runs/norm2
+CUDA_VISIBLE_DEVICES=1 python pdseg/train.py --cfg ./configs/sf-hr18-size-9.yaml --do_eval | tee -a ./runs/size9
+CUDA_VISIBLE_DEVICES=1 python pdseg/train.py --cfg ./configs/sf-hr18-norm-3.yaml --do_eval | tee -a ./runs/norm3
 
 # CUDA_VISIBLE_DEVICES=0 python pdseg/vis.py --cfg ./configs/sf-hr18-city-1.yaml --use_gpu
