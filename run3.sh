84773a24d56e37e7172843bc725e9a0be4df0155
brycezxl <43881818+brycezxl@users.noreply.github.com>
2020-11-10 14:44:00 +0800

    Update run3.sh

--- run3.sh
+++ run3.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr18-agg-1.yaml --do_eval | tee -a ./runs/agg1
-CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr18-agg-2.yaml --do_eval | tee -a ./runs/agg2
-CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr18-agg-3.yaml --do_eval | tee -a ./runs/agg3
+CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr48-agg-1.yaml --do_eval | tee -a ./runs/agg1
+CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr48-agg-2.yaml --do_eval | tee -a ./runs/agg2
+CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr48-agg-3.yaml --do_eval | tee -a ./runs/agg3
