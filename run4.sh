d6a7837eb7d939d243370742c401b589402fc49f
brycezxl <43881818+brycezxl@users.noreply.github.com>
2020-11-15 14:46:32 +0800

    1

--- run4.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,2 python pdseg/train.py --cfg ./configs/sf-hr48-agg-4.yaml --do_eval | tee -a ./runs/agg4
-CUDA_VISIBLE_DEVICES=0,2 python pdseg/train.py --cfg ./configs/sf-hr48-agg-5.yaml --do_eval | tee -a ./runs/agg5
-CUDA_VISIBLE_DEVICES=0,2 python pdseg/train.py --cfg ./configs/sf-hr48-agg-6.yaml --do_eval | tee -a ./runs/agg6
