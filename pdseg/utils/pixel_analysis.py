667b8ed6f01d20e88e6fce64afc3be911b138a0a
brycezxl <43881818+brycezxl@users.noreply.github.com>
2020-11-15 14:43:43 +0800

    1

--- pdseg/utils/pixel_analysis.py
+++ pdseg/utils/pixel_analysis.py
@@ -2,38 +2,37 @@ import os
 import tqdm
 import cv2
 import numpy as np
+import random
 
 
 def print_label(label, com):
     print(com, end=" ")
     total = 0
-    for m in range(1, len(label)):
+    for m in range(len(label)):
         total += label[m]
-    for m in range(1, len(label)):
-        print("%d: %.2f%% " % (m, label[m] / total * 100), end="")
+    for m in range(len(label)):
+        print("%d: %.6f%% " % (m, label[m] / total * 100), end="")
     print("")
 
 
 # path = r"D:\Projects\hualu-laneline-detection\dataset\hualu_laneline\testB\test"
-path = r"/home/zxl/hualu-laneline-detection/data/train_label"
-path1 = r"/home/zxl/hualu-laneline-detection/data/train_pre_tag"
+path1 = r"/home/zxl/hualu-laneline-detection/data/train_label"
+path2 = r"/home/zxl/hualu-laneline-detection/data/train_pre_tag"
 label_dic = np.zeros(20)
 occur_dic = np.zeros(20)
-pic_list = os.listdir(path)
-pic_list += os.listdir(path1)
 
-for n, p in tqdm.tqdm(enumerate(pic_list), total=len(pic_list)):
-    image = cv2.imread(os.path.join(path, p), cv2.IMREAD_GRAYSCALE)
-    # image = cv2.resize(image, (int(image.shape[0] / 2), int(image.shape[1] / 2)), interpolation=cv2.INTER_CUBIC)
-    tmp = np.zeros(20)
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            k = image[i, j]
-            if k != 0:
+for (path, pic_list) in [(path1, os.listdir(path1)), (path2, os.listdir(path2))]:
+    random.shuffle(pic_list)
+    for n, p in tqdm.tqdm(enumerate(pic_list), total=len(pic_list)):
+        image = cv2.imread(os.path.join(path, p), cv2.IMREAD_GRAYSCALE)
+        # image = cv2.resize(image, (int(image.shape[0] / 2), int(image.shape[1] / 2)), interpolation=cv2.INTER_CUBIC)
+        tmp = np.zeros(20)
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
                 tmp[image[i, j]] += 1
-    label_dic += tmp
-    occur_dic += [1 if tmp[i] > 0 else 0 for i in range(20)]
-    if (n + 1) % 50 == 0:
-        print("%d / %d" % (n + 1, len(pic_list)))
-        print_label(label_dic, "[pixel]")
-        print_label(occur_dic, "[occur]")
+        label_dic += tmp
+        occur_dic += [1 if tmp[i] > 0 else 0 for i in range(20)]
+        if (n + 1) % 200 == 0:
+            print("%d / %d" % (n + 1, len(pic_list)))
+            print_label(label_dic, "[pixel]")
+            print_label(occur_dic, "[occur]")
--- run5.sh
+++ run5.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr48-agg-8.yaml --do_eval | tee -a ./runs/agg8
-CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr48-agg-9.yaml --do_eval | tee -a ./runs/agg9
+CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr48-weight-1.yaml --do_eval | tee -a ./runs/w1
+CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr48-weight-2.yaml --do_eval | tee -a ./runs/w2
 
 # CUDA_VISIBLE_DEVICES=0 python pdseg/vis.py --cfg ./configs/sf-hr18-city-1.yaml --use_gpu
