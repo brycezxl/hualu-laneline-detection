4eb052a262b60410f1977f068b0bd314a29e3f16
brycezxl <43881818+brycezxl@users.noreply.github.com>
2020-11-16 15:20:45 +0800

    1

--- run1.sh
+++ run1.sh
@@ -1,4 +1,5 @@
-CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr48-weight-2.yaml --do_eval | tee -a ./runs/w2
-CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr48-weight-3.yaml --do_eval | tee -a ./runs/w3
+CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr64-agg-1.yaml --do_eval | tee -a ./runs/641
+CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr64-agg-2.yaml --do_eval | tee -a ./runs/642
+CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr64-agg-3.yaml --do_eval | tee -a ./runs/643
 
 # CUDA_VISIBLE_DEVICES=0 python pdseg/vis.py --cfg ./configs/sf-hr18-city-1.yaml --use_gpu
