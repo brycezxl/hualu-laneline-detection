cd7c969a9555a999ac3aecbec8177c14438e4218
brycezxl <43881818+brycezxl@users.noreply.github.com>
2020-11-17 14:59:59 +0800

    1

--- run2.sh
+++ run2.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-loss-2.yaml --do_eval | tee -a ./runs/laneloss2
-CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-loss-3.yaml --do_eval | tee -a ./runs/laneloss3
+CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-agg-1.yaml --do_eval | tee -a ./runs/18agg11
+CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-agg-2.yaml --do_eval | tee -a ./runs/18agg2
