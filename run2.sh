98d195f4aca68fbeed3c383143857b72ec67722e
brycezxl <43881818+brycezxl@users.noreply.github.com>
2020-11-18 20:57:37 +0800

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -1,3 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-step-1.yaml --do_eval | tee -a ./runs/step1
-CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-step-2.yaml --do_eval | tee -a ./runs/step2
-CUDA_VISIBLE_DEVICES=0 python pdseg/train.py --cfg ./configs/sf-hr18-step-3.yaml --do_eval | tee -a ./runs/step3
+CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr48-final-1.yaml --do_eval | tee -a ./runs/final1
+CUDA_VISIBLE_DEVICES=0,1 python pdseg/train.py --cfg ./configs/sf-hr48-final-2.yaml --do_eval | tee -a ./runs/final2
